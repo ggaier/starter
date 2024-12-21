3924235d72d5ad1169c8d6332fdf70810e9482d4
wenbo <jwenbo52@gmail.com>
2024-12-21 22:34:48 +0800

    my nachad configs

--- init.lua
+++ init.lua
@@ -26,8 +26,17 @@ require("lazy").setup({
 }, lazy_config)
 
 -- load theme
+
+-- put this after lazy setup 
+
+-- (method 1, For heavy lazyloaders)
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
+
+-- (method 2, for non lazyloaders) to load all highlights at once
+for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+  dofile(vim.g.base46_cache .. v)
+end
 
 require "options"
 require "nvchad.autocmds"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,25 @@ return {
     end,
   },
 
+  "nvim-lua/plenary.nvim",
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  "nvchad/volt",
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
